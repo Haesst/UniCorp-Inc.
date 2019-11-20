864f8a6a900c5774110e46c33ff04857eb53066a
Haesst <robin.ortryd@futuregames.nu>
2019-11-20 12:50:00 +0100

    Changed EnemyState::Idle to just move them diagonally in testing purposes. Added the flag "SDL_RENDERER_VSYNC" to only render the amount of frames necessary

--- UnicornEngine/MyGame/EnemyState.cpp
+++ UnicornEngine/MyGame/EnemyState.cpp
@@ -1,4 +1,5 @@
 #include "EnemyState.h"
+#include "Enemy.h"
 #include <iostream>
 
 void EnemyState::Idle::Enter(Enemy* entity)
@@ -7,7 +8,8 @@ void EnemyState::Idle::Enter(Enemy* entity)
 
 void EnemyState::Idle::Execute(Enemy* entity)
 {
-	std::cout << "Enemy idle." << std::endl;
+	FG::Vector2D curPos = entity->GetPosition();
+	entity->SetPosition(FG::Vector2D(curPos.x + 1, curPos.y + 1));
 }
 
 void EnemyState::Idle::Exit(Enemy* entity)
